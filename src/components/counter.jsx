76902a5ff059537f4ff918c34df035efcf29ffb2
K. H <knhg2013@gmail.com>
2019-03-08 22:38:57 -0500

    1)Add Rendering Class Dynamically 1)Add Rendering Lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   styles = {
@@ -13,12 +14,23 @@ class Counter extends Component {
   render() {
     return (
       <React.Fragment>
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    return classes;
   }
 
   formatCount() {
